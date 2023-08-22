d1c45f9253c623879f881796b128845825f372e9
amnonbb <amnonbb@gmail.com>
2023-08-22 13:39:32 +0300

    Use id in live-proxy app

--- src/components/Service.js
+++ src/components/Service.js
@@ -81,7 +81,7 @@ class Service extends Component {
                     </Menu.Item>
                     <Menu.Item>
                         <Label size='big' color='grey'>
-                            {description || id}
+                            {this.props.id === "live-proxy" ? id : description || id}
                         </Label>
                     </Menu.Item>
                     <Menu.Item>
